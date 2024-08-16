e43e929431852c90338bf13f2530e22e8f8f9ff7
krnrmnk <karinrom.2000@gmail.com>
2024-08-17 02:09:09 +0300

    Домашнее задание 5
    Создан генератор предсказаний.

--- script.js
+++ script.js
@@ -13,3 +13,40 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const predictions = [
+    'Сегодня тебя ждет что-то неожиданное!',
+    'Получишь хорошие новости в ближайшем будущем!',
+    'Тебя ждет интересное знакомство!',
+    'Удача сегодня будет на твоей стороне!',
+    'Сегодня обязательно произойдет что-то хорошее!'
+];
+
+const forecastButton = document.querySelector('.forecast-btn');
+const currentForecast = document.querySelector('.current-forecast h1');
+const currentPersentage = document.querySelector('.current-forecast p');
+const forecastList = document.querySelector('.forecasts');
+const forecastTemplate = document.getElementById('forecast-item').content;
+
+forecastButton.addEventListener('click', () => {
+    const predictionIndex = Math.floor(Math.random() * predictions.length);
+    const predictionText = predictions[predictionIndex];
+    const eventProbability = generatePrediction(0, 100);
+
+    currentForecast.textContent = predictionText;
+    currentPersentage.textContent = `Вероятность: ${eventProbability}%`;
+
+    addNewForecast(predictionText, eventProbability);
+});
+
+function generatePrediction(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function addNewForecast(predictionText, eventProbability) {
+    const forecastItem = document.importNode(forecastTemplate, true);
+    forecastItem.querySelector('h3').textContent = predictionText;
+    forecastItem.querySelector('p').textContent = `Вероятность: ${eventProbability}%`;
+
+    return forecastList.prepend(forecastItem);
+}
